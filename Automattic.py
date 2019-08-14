55ac23a1ec54e1ed95e152edd9d588d4f0beb392
Forrest Smith <forrest216@gmail.com>
2019-08-14 03:13:09 -0700

    add color information to BONUS data

--- Automattic.py
+++ Automattic.py
@@ -10,13 +10,20 @@ with open('data.json') as f:
 with open('airport_data.json') as f:
     airports = json.load(f)
 
+with open('colors.json') as f:
+    colors = json.load(f)
+
 for city in data:
     cities.append(city['host'])
 
 unique = set(cities)
 
 for city in unique:
-    cities_with_names.append({"code": city.upper(), "city_name": airports[city.upper()]})
+    cities_with_names.append({"code": city.upper(), "city_name": airports[city.upper()], "color": colors[city]})
+
+# {'ams', 'tpe', 'sin', 'syd', 'den', 'atl', 'sea', 'dca', 'mxp', 'sjc', 
+# 'mia', 'ewr', 'gru', 'bur', 'vie', 'cdg', 'dfw', 'bom', 'hkg', 'arn', 
+# 'lhr', 'fra', 'jnb', 'ord', 'yyz', 'nrt', 'mad', 'kix'}
 
 with open('BONUS_data.json', 'w') as writeJSON:
    json.dump(cities_with_names, writeJSON, ensure_ascii=False)
